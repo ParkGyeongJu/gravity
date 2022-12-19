a220e4380d280ee1776d509ffc03ff0a8e8895ae
ParkGyeongJu <anauddl414@naver.com>
2022-12-19 20:28:56 +0900

    file

---
+++ sketch.js
@@ -0,0 +1,60 @@
+let ball;
+
+function setup() {
+  createCanvas(400, 200);
+
+  ball = new Particle();
+}
+
+
+function draw() {
+  background(220);
+
+  let gravity = createVector(0, 0.1);
+  ball.addForce(gravity);
+
+  ball.update();
+  ball.show();
+}
+
+
+class Particle {
+  constructor() {
+    this.pos = createVector(width/2, height/2);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+
+    this.c = color(0);
+    this.w = 50;
+  }
+
+  addForce(aForce) {
+    this.acc.add(aForce);
+  }
+
+  update() {
+    this.checkEdge();
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+
+    this.acc.set(0, 0);
+  }
+
+
+  checkEdge() {
+    if (this.pos.y > height) {
+      this.vel.y = this.vel.y * -0.8;
+      this.pos.y = height;
+    }
+    
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+    }
+  }
+
+
+  show() {
+    fill(this.c);
+    ellipse(this.pos.x, this.pos.y, this.w, this.w);
+  }
+}
